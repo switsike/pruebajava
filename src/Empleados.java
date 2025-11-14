83ed5e3fc7ad4f1b2a06c6f82d6d501cfd7ae137
switsike <L25171003@culiacan.tecnm.mx>
2025-11-14 03:12:53 +0000

    Agregue para la impresion de salida

--- src/Empleados.java
+++ src/Empleados.java
@@ -2,27 +2,31 @@
 public class Empleados
 {
 	public static void main(String[] args) {
-		int horastrabajadas=0;
 		int i;
-		double suma=0;
-		Empleado[] empleados= new Empleado[3];
-		empleados[0]= new Empleado("daniel",0);
-		empleados[1]= new Empleado("Victor",2);
-		empleados[2]= new Empleado("Jesus",6);
+		double suma = 0;
+		int c=0;
+		double promedio=0;
+		Empleado[] empleados = new Empleado[3];
+		empleados[0] = new Empleado("daniel", 0);
+		empleados[1] = new Empleado("Victor", 2);
+		empleados[2] = new Empleado("Jesus", 6);
 
-		System.out.println("Longitud de la array: "+Empleado.length);
-		for(i=0;i<Empleado.length;i++)
-		{
-			Empleado empleadoactual= empleados[i];
-			suma=suma+empleadoactual.getHoras();
+		System.out.println("Longitud de la array: " + empleados.length);
+		for (i = 0; i < empleados.length; i++) {
+			Empleado empleadoactual = empleados[i];
+			suma = suma + empleadoactual.getHoras();
 		}
 
-		for(i=0;i<empleados.length;i++)
-		{
-			System.out.println("Empleado "+(i+1)+": "+empleadoactual.getnombre()+" Horas trabajadas: "+empleadoactual.getHoras()+" Se le pagara: "+(empleadoactual.getHoras()*50)+" Pesos");
-			
+		for (i = 0; i < empleados.length; i++) {
+			Empleado empleadoactual = empleados[i];
+			promedio= suma / empleados.length;
+			System.out.println("Empleado " + (i + 1) + ": " + empleadoactual.getNombre() + " Horas trabajadas: " + empleadoactual.getHoras() + " Se le pagara: " + (empleadoactual.getHoras() * 50) + " Pesos");
+			if(empleadoactual.getHoras()>promedio){
+				c++;
+			}
 		}
-		System.out.printf("promedio de horas trabajadas: %.2f",suma/3);
+		System.out.printf("promedio de horas trabajadas: %.2f\n", promedio);
+		System.out.printf("Cantidad de empleados que superan el promedio de horas trabajadas: %d\n", c);
 
 		
 	}
